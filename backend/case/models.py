9e9a22ea4ea7c49e7855af10dbebd358e1f79e37
preytor <pgrfpgm@gmail.com>
2023-09-19 12:07:14 -0400

    Added new parameters to the Case

--- backend/case/models.py
+++ backend/case/models.py
@@ -13,6 +13,15 @@ class Case(models.Model):
         ("In progress", "In progress"),
         ("Closed", "Closed"),
     ])
+    borrowed_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    payment_frequency = models.CharField(max_length=50, choices=[
+        ("Weekly", "Weekly"),
+        ("Bi-weekly", "Bi-weekly"),
+        ("Monthly", "Monthly"),
+    ], null=True)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    amortization_period = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     class Meta:
         db_table = "case"
