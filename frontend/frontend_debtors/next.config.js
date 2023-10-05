7e98b757a4872ec9ab32785e796f742d58a75c05
preytor <pgrfpgm@gmail.com>
2023-10-05 15:55:19 -0400

    whitelisted xsgames to get random profiles from it

--- frontend/frontend_debtors/next.config.js
+++ frontend/frontend_debtors/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['reactstrap'],
+    },
+    images: {
+        domains: ['xsgames.co'],
+    },
+}
 
 module.exports = nextConfig
