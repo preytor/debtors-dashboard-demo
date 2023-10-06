f4737dc3d4dd95be370a944084ecc9bfa1d7e9ca
preytor <pgrfpgm@gmail.com>
2023-10-06 14:30:58 -0400

    modified the sidebar to look better

--- frontend/frontend_debtors/src/app/(dashboardLayout)/layouts/sidebar/SideBar.tsx
+++ frontend/frontend_debtors/src/app/(dashboardLayout)/layouts/sidebar/SideBar.tsx
@@ -1,58 +1,60 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import { Button, Nav, NavItem, NavLink } from 'reactstrap';
 import styles from './sidebar.module.css';
 
-import React from 'react';
-import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
-  CDBAccordion,
-} from 'cdbreact';
-import { NavLink } from 'reactstrap';
+import classNames from "classnames";
+import Logo from '../shared/logo';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { 
+  GrUserManager,
+  GrUser,
+  GrDocumentUser
+} from 'react-icons/gr';
 
 export default function SideBar() {
   return (
-    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-    <CDBSidebar textColor="#fff" backgroundColor="#333" className={''} breakpoint={0} toggled={false} minWidth={''} maxWidth={''}>
-      <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-        <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-          Sidebar
-        </a>
-      </CDBSidebarHeader>
-
-      <CDBSidebarContent className="sidebar-content">
-        <CDBSidebarMenu>
-          <NavLink to="/">
-            <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink to="/tables">
-            <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink to="/profile">
-            <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
-          </NavLink>
-          <NavLink to="/analytics">
-            <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
-          </NavLink>
-
-          <NavLink to="/hero404" target="_blank">
-            <CDBSidebarMenuItem icon="exclamation-circle">404 page</CDBSidebarMenuItem>
-          </NavLink>
-        </CDBSidebarMenu>
-      </CDBSidebarContent>
-
-      <CDBSidebarFooter className={styles.navfooter}>
-        <div
-          style={{
-            padding: '20px 5px',
-          }}
+    <aside className={styles.sidebar + "flex flex-col justify-betweeen h-screen p-4"}>
+      <div className="flex flex-col items-center">
+        <Logo />
+        <Nav 
+          className='mb-4' 
+          vertical
+          pills
         >
-          Sidebar Footer
-        </div>
-      </CDBSidebarFooter>
-    </CDBSidebar>
-  </div>
+          <Link href="/workers">
+          <NavItem className="mb-2 hover:bg-gray-200 rounded-full py-2 px-4 flex items-center space-x-2 text-xl" label='Workers'>
+          <GrUserManager className="text-xl" />
+            Workers
+          </NavItem> 
+          </Link>
+          <Link href="/debtors">
+          <NavItem className="mb-2 hover:bg-gray-200 rounded-full py-2 px-4 flex items-center space-x-2 text-xl" label='Debtors'>
+          <GrUser className="text-xl" />
+            Debtors
+          </NavItem> 
+          </Link>
+          <Link href="/cases">
+          <NavItem className="mb-2 hover:bg-gray-200 rounded-full py-2 px-4 flex items-center space-x-2 text-xl" label='Cases'>
+          <GrDocumentUser className="text-xl" />
+            Cases
+          </NavItem> 
+          </Link>
+        </Nav>
+      </div>
+      <button className={styles.sideButton + " bg-white rounded-full border border-gray-200 text-gray-800 px-4 py-2 flex items-center space-x-2 hover:bg-gray-200"}>
+        <Image
+          width={36}
+          height={36}
+          className="rounded-full"
+          src="https://xsgames.co/randomusers/avatar.php?g=male"
+          alt="User profile"
+        />
+        <span>Profile</span>
+      </button>
+    </aside>
   );
 };
