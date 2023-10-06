f80b24dd1d5b0a29f5d7127cb533fd9cbf3bca62
preytor <pgrfpgm@gmail.com>
2023-10-06 14:30:07 -0400

    added experimental server actions

--- frontend/frontend_debtors/next.config.js
+++ frontend/frontend_debtors/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ['reactstrap'],
+        serverActions: true,
     },
     images: {
         domains: ['xsgames.co'],
