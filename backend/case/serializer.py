63e980ffde557c03ee491cbac38f574a4b7ad4be
preytor <pgrfpgm@gmail.com>
2023-10-09 11:42:16 -0400

    Added alkl of the fields in the serializer

--- backend/case/serializer.py
+++ backend/case/serializer.py
@@ -3,6 +3,15 @@ from rest_framework import serializers
 from .models import Case
 
 class CaseSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Case
-        fields = ('id', 'debtor', 'assigned_worker', 'case_status')
+        fields = ('id', 
+                  'assigned_worker',
+                  'debtor',
+                  'case_status', 
+                  'borrowed_amount', 
+                  'payment_frequency', 
+                  'interest_rate', 
+                  'amortization_period', 
+                  'created_at')
