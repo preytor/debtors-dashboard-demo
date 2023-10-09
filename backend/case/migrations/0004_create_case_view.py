3ff8906d9f8c91273af3d57af511819c6e6c5d1d
preytor <pgrfpgm@gmail.com>
2023-10-09 12:35:58 -0400

    added migration files and corrected the case id

--- backend/case/migrations/0004_create_case_view.py
+++ backend/case/migrations/0004_create_case_view.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
             """
             CREATE OR REPLACE VIEW case_view AS
             SELECT
-                c.id,
+                c.id as case_id,
                 c.assigned_worker_id,
                 w.name AS assigned_worker_name,
                 c.debtor_id,
