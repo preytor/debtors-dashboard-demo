5771cf5ac50468d70bf94aa6901cbfd692b692ae
preytor <pgrfpgm@gmail.com>
2023-10-09 11:54:39 -0400

    Added a view for cases

---
+++ backend/case/migrations/0004_create_case_view.py
@@ -0,0 +1,32 @@
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('case', '0003_alter_case_case_status'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            """
+            CREATE OR REPLACE VIEW case_view AS
+            SELECT
+                c.id,
+                c.assigned_worker_id,
+                w.name AS assigned_worker_name,
+                c.debtor_id,
+                d.name AS debtor_name,
+                c.case_status,
+                c.borrowed_amount,
+                c.payment_frequency,
+                c.interest_rate,
+                c.amortization_period,
+                c.created_at
+            FROM debtors.case c
+            LEFT JOIN worker w ON c.assigned_worker_id = w.id
+            LEFT JOIN worker d ON c.debtor_id = d.id;
+            """
+        ),
+    ]
