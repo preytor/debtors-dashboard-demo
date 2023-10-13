872dcba6f3f293ce15f57e8bc419ffe5b1585690
preytor <pgrfpgm@gmail.com>
2023-10-13 15:37:10 -0400

    added the default pages for cases, debtors and workers, modified the main page for dummy

--- frontend/frontend/src/app/page.tsx
+++ frontend/frontend/src/app/page.tsx
@@ -2,9 +2,8 @@ import { Button } from "@nextui-org/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      test
-
+    <div className="">
+      main page
     </div>
   )
 }
