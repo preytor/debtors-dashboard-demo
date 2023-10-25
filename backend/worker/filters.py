3bb5f983c0b8295363efe41f35ee271d68635b66
preytor <pgrfpgm@gmail.com>
2023-10-25 12:57:45 -0400

    Modified the roles to accept multiple options

--- backend/worker/filters.py
+++ backend/worker/filters.py
@@ -5,14 +5,14 @@ class WorkerFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         name = request.query_params.get('name', None)
         contact_info = request.query_params.get('contact_info', None)
-        role = request.query_params.get('role', None)
+        roles = request.query_params.getlist('role', None)
         ordering = request.query_params.get('ordering', None)
         if name:
             queryset = queryset.filter(name__icontains=name)
         if contact_info:
             queryset = queryset.filter(contact_info__icontains=contact_info)
-        if role:
-            queryset = queryset.filter(role=role)
+        if roles:
+            queryset = queryset.filter(role__in=roles)
         if ordering:
             queryset = queryset.order_by(ordering)
         else:
