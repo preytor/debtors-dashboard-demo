c9ca61f103f9d2e90d81332c1ba81de657436e5d
preytor <pgrfpgm@gmail.com>
2023-10-25 16:20:02 -0400

    Added a create and delete worker function

--- frontend/frontend-angular/src/app/services/workers.service.ts
+++ frontend/frontend-angular/src/app/services/workers.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
-import { WorkerResults } from '../interfaces/worker.results';
+import { WorkerResults } from '../interfaces/worker/worker.results';
 import { SortDirection } from '@angular/material/sort';
-import WorkerSearch from '../interfaces/worker.search';
+import WorkerSearch from '../interfaces/worker/worker.search';
+import { WorkerForm } from '../interfaces/worker/worker.form';
 
 @Injectable({
   providedIn: 'root'
@@ -27,11 +28,26 @@ export class WorkersService {
     if (args.contact_info) {
       final_args += `&contact_info=${args.contact_info}`;
     }
-    if (args.role) {
-      final_args += `&role=${args.role}`;
+    if (args.roles) {
+      for (let role of args.roles) {
+        final_args += `&roles=${role}`;
+      }
     }
-    console.log(`${environment.backend_url}/api/workers?page=${page+1}&page_size=${perPage}${final_args}`)
+    
     const response = this.http.get<WorkerResults>(`${environment.backend_url}/api/workers?page=${page+1}&page_size=${perPage}${final_args}`);
     return response;
+  }
+
+  createWorker(workerData: WorkerForm): Observable<any> {
+    let workerFormData = new FormData();
+    workerFormData.append('name', workerData.name);
+    workerFormData.append('contact_info', workerData.contact_info);
+    workerFormData.append('role', workerData.role);
+
+    return this.http.post(`${environment.backend_url}/api/worker`, workerFormData);
+  }
+
+  deleteWorker(workerId: number): Observable<any> {
+    return this.http.delete(`${environment.backend_url}/api/worker/${workerId}`);
   }
 }
